baf82b03a802815f23e9f0fc5b354d6dd6704dc8
maundu55 <marymaundu.mm@gmail.com>
2025-02-18 14:49:59 +0300

    add path routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/collection' element={<Collection />}/>
+      </Routes>
+    </div>
   )
 }
 
